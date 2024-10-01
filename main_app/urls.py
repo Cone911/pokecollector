97e111563860dbe36cf60da7652e218a632035d4
JP_Escobari <jpe911@gmail.com>
2024-10-01 15:57:53 -0400

    Add Pokemon Detail Page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views # Import views to connect routes to view functions
+from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  # route for cats index
-  path('pokemon/', views.index, name='cat-index'),
+  path('pokemon/', views.index, name='pokemon-index'),
+  path('pokemon/<int:poke_id>/', views.poke_detail, name='poke-detail')
 ]
